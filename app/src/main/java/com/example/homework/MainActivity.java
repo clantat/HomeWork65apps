af863974c18d4f4a13f61563bd7d5812015c061f
idact <elementalofspace@gmail.com>
2019-10-28 09:49:23 +0300

    fixed issues

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -3,28 +3,29 @@ package com.example.homework;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import javax.inject.Inject;
 
 import ru.terrakok.cicerone.Navigator;
 import ru.terrakok.cicerone.NavigatorHolder;
+import ru.terrakok.cicerone.Router;
 import ru.terrakok.cicerone.android.support.SupportAppNavigator;
 import ru.terrakok.cicerone.commands.Command;
-import ru.terrakok.cicerone.commands.Replace;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
+    Router router;
+    @Inject
     NavigatorHolder navigatorHolder;
 
-    private Navigator navigator = new SupportAppNavigator(this,R.id.container){
+    private Navigator navigator = new SupportAppNavigator(this, R.id.container) {
         @Override
         public void applyCommands(Command[] commands) {
             super.applyCommands(commands);
-            getSupportFragmentManager().executePendingTransactions();
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         MyApp.get().getAppComponent().inject(this);
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             if (savedInstanceState != null) {
                 return;
             }
-            navigator.applyCommands(new Command[]{new Replace(new Screens.ContactsScreen(1))});
+            router.newRootScreen(new Screens.ContactsScreen(1));
         }
     }
 
